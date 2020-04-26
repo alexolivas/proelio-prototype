9fd767d2c1b8bc09735e6a20e2d22531dbb0624d
Alex Olivas <alex.olivas11@gmail.com>
2020-04-26 16:19:59 -0400

    Updates

---
+++ precache-manifest.087b6d9b2303baa16a8c7b0ea7ad8ba3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d6cc799729277ed64d8877107b6d5a",
+    "url": "/proelio-prototype/index.html"
+  },
+  {
+    "revision": "d85311af716dd93b8aa3",
+    "url": "/proelio-prototype/static/css/2.3f320bb4.chunk.css"
+  },
+  {
+    "revision": "5d6df1a30d7b7be6e1b7",
+    "url": "/proelio-prototype/static/css/main.7f0d1af2.chunk.css"
+  },
+  {
+    "revision": "d85311af716dd93b8aa3",
+    "url": "/proelio-prototype/static/js/2.7dbae67e.chunk.js"
+  },
+  {
+    "revision": "f17ba51ff6393f34eef1fb937eeee15d",
+    "url": "/proelio-prototype/static/js/2.7dbae67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6df1a30d7b7be6e1b7",
+    "url": "/proelio-prototype/static/js/main.9aba9a2f.chunk.js"
+  },
+  {
+    "revision": "86ab52e00c2c1dc5ec61",
+    "url": "/proelio-prototype/static/js/runtime-main.86eb4796.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/proelio-prototype/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/proelio-prototype/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/proelio-prototype/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/proelio-prototype/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/proelio-prototype/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/proelio-prototype/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/proelio-prototype/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/proelio-prototype/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/proelio-prototype/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/proelio-prototype/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/proelio-prototype/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/proelio-prototype/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/proelio-prototype/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/proelio-prototype/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/proelio-prototype/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proelio-prototype/static/media/logo.5d5d9eef.svg"
+  }
+]);
